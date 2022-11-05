fdd4e96c370519f949af31573c627d9897ae8928
Евгений Калюжный <e.kalyuzhnyy@inbox.ru>
2022-11-05 17:17:26 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def start_training(char_name: str, char_class: str) -> str:
           'defence — чтобы блокировать атаку противника или special — '
           'чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd: str = None
+    cmd = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -105,6 +105,3 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
